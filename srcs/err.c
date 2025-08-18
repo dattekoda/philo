c6c3712afa5c532ef80aa93f3168d1480c66817d
Kodai Hanadate <khanadat@K.local>
2025-08-18 13:00:32 +0900

    nya

--- srcs/err.c
+++ srcs/err.c
@@ -6,7 +6,7 @@
 /*   By: khanadat <khanadat@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 11:10:47 by khanadat          #+#    #+#             */
-/*   Updated: 2025/08/18 11:36:41 by khanadat         ###   ########.fr       */
+/*   Updated: 2025/08/18 12:26:45 by khanadat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 
 void	exit_philo(t_philo *philo, char *str)
 {
+	int	i;
+
+	i = -1;
+	while (++i < philo->data->num_philo)
+		pthread_mutex_unlock(&philo->data->fork_mutex[i]);
+	i = -1;
+	while (++i < philo->data->num_philo)
+		pthread_mutex_destroy(&philo->data->fork_mutex[i]);
 	if (!str)
 		ft_putendl_err(str);
 	exit(ERR);
